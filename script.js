74a2a4ea76af7bbac0909bd2feb45412c350c5aa
SigneWehlast <sswe48620@edu.ucl.dk>
2023-04-13 11:41:02 +0200

    signes javascript

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
 //Kasper
-let i = 0;
-let billeder = [];
-let tid = 3000;
 
-billeder[0] = 'gul.png'
-billeder[1] = 'gul.png'
-billeder[2] = 'grøn.png'
-
-function changeImg(){
-    document.slide.src= billeder[i];
-
-    if(i < billeder.length - 1){
-        i++;
-    } else {
-        i = 0;
-    }
-
-setTimeout("ændrebilleder()", time)
-}
-
-window.onload = ændrebilleder;
 
 //Signe
 let countDownBowling = new Date("May 5, 2023 17:00:00").getTime();
@@ -32,9 +12,9 @@ let countDown = setInterval(function() {
 
     let dage = Math.floor(dageIndtil / (1000 * 60 * 60 * 24));
 
-    let timer = Math.floor((dageIndtil % (1000 * 60 * 60 * 24)) / (1000 * 60 60));
+    let timer = Math.floor((dageIndtil % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 
-    let minutter = Math.floor((dageIndtil % (1000 60 * 60)) / (1000 * 60));
+    let minutter = Math.floor((dageIndtil % (1000 * 60 * 60)) / (1000 * 60));
 
 
     if (dageIndtil < 0 ) {
