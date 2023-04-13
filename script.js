952eddcdfdebadcd7e7f1cc8062c40498291f17f
cloud14 <119939674+Abdirahman4599@users.noreply.github.com>
2023-04-13 11:21:27 +0200

    js og andet

--- script.js
+++ script.js
@@ -57,3 +57,19 @@ function show_hide() {
    }
 }
 //Claudia
+
+const myForm = document.getElementById("myForm");
+const popup = document.getElementById("popup");
+const userNameSpan = document.getElementById("userName");
+
+myForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+
+  if (nameInput.checkValidity() && emailInput.checkValidity()) {
+    userNameSpan.innerText = nameInput.value;
+    popup.style.display = "block";
+  }
+});
