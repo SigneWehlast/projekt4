69356ccc49bbcd6f44b15339845b1081a02adcd4
cloud14 <119939674+Abdirahman4599@users.noreply.github.com>
2023-04-13 11:33:12 +0200

    js signe

--- script.js
+++ script.js
@@ -24,27 +24,34 @@ window.onload = ændrebilleder;
 //Signe
 let countDownBowling = new Date("May 5, 2023 17:00:00").getTime();
 
-let x = setInterval(function() {
+let countDown = setInterval(function() {
 
-    let nu = new Date().getTime();
+    let iDag = new Date().getTime();
 
-    let dageIndtil = countDownBowling - nu;
+    let dageIndtil = countDownBowling - iDag;
 
     let dage = Math.floor(dageIndtil / (1000 * 60 * 60 * 24));
 
-    let timer = Math.floor((dageIndtil % (1000 * 60 * 60 * 24)) / (1000 * 60 *60));
+    let timer = Math.floor((dageIndtil % (1000 * 60 * 60 * 24)) / (1000 * 60 60));
 
-    let minutter = Math.floor((dageIndtil % (1000 * 60 * 60 * 24)) / (1000 * 60));
+    let minutter = Math.floor((dageIndtil % (1000 60 * 60)) / (1000 * 60));
 
-    let sekunder = Math.floor((dageIndtil % (1000 * 60)) / 1000);
-
-    document.getElementById("countdownid").innerHTML = dage + "d " + timer + "t " + minutter + "m " + sekunder + "s ";
 
     if (dageIndtil < 0 ) {
-        clearInterval(x);
-        document.getElementById("countdownid").innerHTML = "Eventet er igang";
+        clearInterval(countDown);
+        document.getElementById("countdownid").innerHTML = "Eventet er igang!";
     }
+    else {
+        document.getElementById("countdownid").innerHTML = dage + " " + timer + " " +  minutter;
+    }
+
 }, 1000);
+
+let dtmArray = ["Dage", "Timer", "Minutter"];
+
+for(let i = 0; i < dtmArray.length; i++) {
+    document.getElementById("dtm").innerHTML += dtmArray[i] + " ";
+}
 
 //Aimi
 //dropdown vises og gemmes
