62f7f3de8cd4fd1f4b9363eacc46272f22dad434
khni48704 <khni48704@edu.ucl.dk>
2023-04-13 12:11:59 +0200

    forside

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 //Kasper
+let slideIndex = 0;
+showSlides();
 
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // 1000 = 1 sekund
+}
 
 //Signe
 let countDownBowling = new Date("May 5, 2023 17:00:00").getTime();
