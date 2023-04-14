cda019c43a444adf6304cded4bf6ccb5f3e51f58
cloud14 <119939674+Abdirahman4599@users.noreply.github.com>
2023-04-14 09:12:31 +0200

    n

--- java/script.js
+++ java/script.js
@@ -7,14 +7,14 @@ function showSlides() {
   let slides = document.getElementsByClassName("slides");
   let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
+  if (slideIndex > slides.length) {slideIndex = 1}
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 5000); // 1000 = 1 sekund
 }
@@ -60,4 +60,4 @@ function getOption() {
     output = selectElement.options[selectElement.selectedIndex].value;
     document.querySelector('.output').textContent = output;
 }
-//Claudia er i mappen event.js
+//Claudia er i mappen opret.js
