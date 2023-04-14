55e0816b98e76362420d17ced3b474a4018c1f26
cloud14 <119939674+Abdirahman4599@users.noreply.github.com>
2023-04-14 11:20:13 +0200

    opdaterret js

--- java/opret.js
+++ java/opret.js
@@ -1,5 +1,6 @@
 //Claudia
 
+
 const myForm = document.getElementById("myForm");
 const popup = document.getElementById("popup");
 const userNameSpan = document.getElementById("userName");
@@ -16,10 +17,24 @@ myForm.addEventListener("submit", function(event) {
   }
 });
 
+function changeText() {
+  document.getElementsByTagName("h1")[0].innerHTML = "UCLSocial";
+}
+
+function resetText() {
+  document.getElementsByTagName("h1")[0].innerHTML = "Oprettelse af event";
+}
+
+ let colors = ["#FF5722", "#4CAF50", "#2196F3", "#9C27B0", "#00BCD4"];
+
  let closeBtn = document.getElementById("lukbtn");
 
 closeBtn.addEventListener("click", function() {
 
   let popup = document.getElementById("popup");
   popup.style.display = "none";
+
+  let randomColor = colors[Math.floor(Math.random() * colors.length)];
+      lukbtn.style.backgroundColor = randomColor;
+      lukbtn.classList.add("active");
 });
