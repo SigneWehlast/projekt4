9bb3c75eb8bf71214baa0325dcfc87f67ed859d3
khni48704 <khni48704@edu.ucl.dk>
2023-04-14 10:41:37 +0200

    js kasper

--- java/script.js
+++ java/script.js
@@ -19,6 +19,13 @@ function showSlides() {
   setTimeout(showSlides, 5000); // 1000 = 1 sekund
 }
 
+let tekst = ["Tur", "tid", "andet"]
+for(let i = 0; i < tekst.length; i++) {
+  document.getElementById("tekst").innerHTML += tekst[i] + " "; //den printer teksten i som her vil være tur, hvor den så pludser det med et mellemrum hvor variabel i array
+}
+function tekstskifterfarve() {
+  document.getElementById("tekst").style.color = "#00DD00"; //den skal tage Funktionen tekstkiFterFarve og ved et onclick skal den ændre tekst Farve
+}
 //Signe
 
 let countDownBowling = new Date("May 5, 2023 17:00:00").getTime();
