89a5f7c9f2fa8a638518be54e554b63de8f0dc6e
khni48704 <khni48704@edu.ucl.dk>
2023-04-17 09:18:40 +0200

    js

--- java/script.js
+++ java/script.js
@@ -31,7 +31,7 @@ const skovbillede = {
   substantiv1: "Skoven",
   pronomen: "en",
 };
-// Display some data from the object:
+
 document.getElementById("tekstjs").innerHTML =
 skovbillede.substantiv1 + " har " + skovbillede.pronomen + "bro";
 
